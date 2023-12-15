960effeb4d9319189bea3db42a44999d9b029d55
AdNarayan <aditya6104narayan@gmail.com>
2023-12-15 14:38:29 +0530

    Added the logics and functionalities.

--- passport.js
+++ passport.js
@@ -2,9 +2,9 @@ window.passport = new window.immutable.passport.Passport({
     baseConfig: new window.immutable.config.ImmutableConfiguration({
       environment: window.immutable.config.Environment.SANDBOX,
     }),
-    clientId: 'enter-client-id',
-    redirectUri: 'redirect-url',
-    logoutRedirectUri: 'logout-url',
+    clientId: '1R7KZRwaSiA7tpWeMnK8OnhlyaEXP2B7',
+    redirectUri: 'https://liked-redbird-noted.ngrok-free.app/',
+    logoutRedirectUri: 'https://liked-redbird-noted.ngrok-free.app/logout.html',
     audience: 'platform_api',
     scope: 'openid offline_access email transact'
   });
